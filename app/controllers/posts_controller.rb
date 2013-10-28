2f1a4f7a6e052d71108609b0f0d213d27305e413
CMDann <CMDann.Development@gmail.com>
2013-10-28 11:34:35 -0500

    Added listing blog posts, posts are added through the posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,12 @@ class PostsController < ApplicationController
   # Route defined for this index action
   # The URL being /posts
   def index
+    # Fetch all the posts from the Post model
+    # Save this to an instance variable
+
+    @all_posts = Post.order('id DESC')
+
+
   end
   # After this index action executes, rails will load
   # the app/views.posts/index.html.erb
