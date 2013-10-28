2789a70ec63d3093908b5695dcb0e29a35c592a6
CMDann <CMDann.Development@gmail.com>
2013-10-28 10:20:20 -0500

    Added some controllers and views

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,17 @@
+class InfoController < ApplicationController
+
+  # We create these so it is obvious which views exist.
+  # These are empty - this is here for you.
+
+  def index
+  end # Automatically loads the: app/views/index.html.erb
+
+  def about
+  end # Automatically loads the: app/views/about.html.erb
+
+  def contact
+  end # Automatically loads the: app/views/contact.html.erb
+
+
+
+end
